029eb0d0d68b2b78008d0db1d79af0b08d75d232
Michael Bestas <mikeioannina@gmail.com>
2024-07-14 05:59:44 +0530

    sakura: Include lineage FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,7 +78,8 @@ LOC_HIDL_VERSION := 3.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
