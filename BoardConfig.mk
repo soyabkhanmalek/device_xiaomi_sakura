ba19c58e9881067d1b22d9adb0b3a608fa0efbe4
Michael Bestas <mkbestas@lineageos.org>
2024-07-14 06:01:30 +0530

    sakura: Inherit from legacy QTI FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,7 @@ LOC_HIDL_VERSION := 3.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
