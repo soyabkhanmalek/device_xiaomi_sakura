7c864ec33cc85c6e777e65ff57547c19fbf86f5e
Farmsun <129572861+Farmsun@users.noreply.github.com>
2024-08-14 11:58:24 +0530

    Update and rename lineage_sakura.mk to aosp_sakura.mk

--- aosp_sakura.mk
+++ aosp_sakura.mk
@@ -13,12 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common pixelos stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
-PRODUCT_NAME := lineage_sakura
+PRODUCT_NAME := aosp_sakura
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 6 Pro
