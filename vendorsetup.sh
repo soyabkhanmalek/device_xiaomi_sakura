ef523bed91b168f60055f983eacc69df9a473299
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2024-09-09 11:21:54 +0530

    fetch hals from lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,5 @@
 # git clone --depth=1 -b 14 https://github.com/aneeshsvha/kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 
 # HALS
-# rm -rf hardware/xiaomi
-# git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+rm -rf hardware/xiaomi
+git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
