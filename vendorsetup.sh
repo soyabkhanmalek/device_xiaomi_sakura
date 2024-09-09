699345b153e54a5e74cbd03f9b4041382e98f4f3
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2024-09-09 11:18:47 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,12 @@
 # Vendor Tree
-rm -rf vendor/xiaomi
-git clone --depth=1 -b 14 https://github.com/aneeshsvha/vendor_xiaomi_sakura vendor/xiaomi/sakura
-git clone --depth=1 -b 14 https://github.com/aneeshsvha/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+# rm -rf vendor/xiaomi
+# git clone --depth=1 -b 14 https://github.com/aneeshsvha/vendor_xiaomi_sakura vendor/xiaomi/sakura
+# git clone --depth=1 -b 14 https://github.com/aneeshsvha/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
 
 # Kernel Tree
-rm -rf kernel/xiaomi
-git clone --depth=1 -b 14 https://github.com/aneeshsvha/kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
+# rm -rf kernel/xiaomi
+# git clone --depth=1 -b 14 https://github.com/aneeshsvha/kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
 
 # HALS
-rm -rf hardware/xiaomi
-git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+# rm -rf hardware/xiaomi
+# git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
